a468bea963cabd06f7ffccac1a069e853341c08f
Kai <kzheng@student.ethz.ch>
2022-05-28 17:57:57 +0200

    Adds QR scanner + improves MainView

--- DankeUbique/Views/MainView.swift
+++ DankeUbique/Views/MainView.swift
@@ -6,37 +6,71 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct MainView: View {
+    @State private var scannerIsVisible: Bool = false
     
     var body: some View {
-        VStack {
-            VStack(spacing: .du_padding_small) {
-                Text("Danke für die schöne Zeit")
-                    .font(.du_title)
+        ZStack {
+            Color("background")
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                VStack(spacing: .du_padding_small) {
+                    Text("Danke für die schöne Zeit")
+                        .font(.du_title)
+                    
+                    Text("Es war mir eine große Freude, ein Jahr lang Teil von euch gewesen zu sein. Trotz meiner kurzen Zeit habe ich unglaublich viel gelernt und eine sehr coole Zeit gehabt.\nDie Kultur und Atmosphäre ist wirklich fantastisch. Ich bin gespannt auf den geilen Scheiss, der noch kommt!")
+                        .font(.du_text)
+                        .multilineTextAlignment(.center)
+                        .lineSpacing(Font.du_text_linespacing)
+                        .padding(.horizontal, .du_padding_large)
+                        .padding(.bottom, .du_padding_small)
+                    
+                    Image("me")
+                }
+                .padding(.top, .du_padding_small)
                 
-                Text("Es war mir eine große Freude, ein Jahr lang, Teil von euch gewesen zu sein. Trotz meiner kurzen Zeit habe ich unglaublich viel gelernt und eine sehr coole Zeit gehabt.\nDie Kultur und Atmosphäre ist wirklich fantastisch. Ich bin gespannt auf den geilen Scheiss, der noch kommt!")
-                    .font(.du_text)
-                    .multilineTextAlignment(.center)
-                    .lineSpacing(Font.du_text_linespacing)
-                    .padding(.bottom, .du_padding_small)
+                Spacer()
                 
-                Image("me")
+                ZStack {
+                    Image("envelope")
+                    
+                    Button(action: { scannerIsVisible = true }) {
+                        Circle()
+                            .frame(width: 72, height: 72)
+                            .foregroundColor(Color("scan-button"))
+                            .overlay(
+                                VStack(spacing: 0) {
+                                    Image(systemName: "qrcode.viewfinder")
+                                        .font(.system(size: 24, weight: .light))
+
+                                    Text("Scan")
+                                        .font(.du_button)
+                                }
+                            )
+                    }
+                    .accentColor(.black)
+                }
+                .offset(y: -.du_padding_large)
+                
+                Spacer()
+                
+                Image("ubique-logo")
             }
-            .padding(.top, .du_padding_small)
-            
-            Spacer()
-            
-            ZStack {
-                Image("envelope")
-            }
-            .offset(y: -.du_padding_large)
-            
-            Spacer()
-            
-            Image("ubique-logo")
+            .padding(.vertical, .du_padding_small)
         }
-        .padding(.horizontal, .du_padding_large)
-        .padding(.vertical, .du_padding_small)
+        .sheet(isPresented: $scannerIsVisible) {
+            CodeScannerView(codeTypes: [.qr]) { result in
+                switch result {
+                case .success(let result):
+                    print(result.string)
+                case .failure(let error):
+                    print("Scanning failed: \(error.localizedDescription)")
+                }
+            }
+            .edgesIgnoringSafeArea(.all)
+        }
     }
 }
