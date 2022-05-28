1fd38bf2f88721e113e3c53b461cd008646b81ab
Kai <kzheng@student.ethz.ch>
2022-05-28 11:59:10 +0200

    Finishes Setup

--- DankeUbique/Views/DankeUbiqueApp.swift
+++ DankeUbique/Views/DankeUbiqueApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct DankeUbiqueApp: App {
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
---
+++ DankeUbique/Views/MainView.swift
@@ -0,0 +1,15 @@
+//
+//  MainView.swift
+//  DankeUbique
+//
+//  Created by Kai Zheng on 28.05.22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    var body: some View {
+        Image("envelope")
+    }
+}
